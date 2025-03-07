873294435cab393e44e434747c65435494da105f
Saket <saket.s9713@gmail.com>
2025-03-07 20:22:27 +0530

    Kanban Board Project

---
+++ helperInfo.js
@@ -0,0 +1,18 @@
+export const icons = {
+    addIcon: '/assets/add.svg',
+    collapseIcon: '/assets/collapse.svg',
+    dbIcon: '/assets/database.svg',
+    editIcon: '/assets/edit.svg',
+    expandIcon: '/assets/expand.svg',
+    trashIcon: '/assets/trash.svg',
+};
+
+export const priorityDetails = [{'value':1,'priority':'High',color:'#f00'},
+    {'value':2,'priority':'Medium',color:'#ff0'},
+    {'value':3,'priority':'Low',color:'#0f0'}];
+
+export const weekDays = {'0':'Sunday','1':'Monday','2':'Tuesday','3':'Wednesday','4':'Thursday','5':'Friday','6':'Saturday'};
+
+export const months = {'0':'Jan','1':'Feb','2':'March','3':'April','4':'May','5':'June','6':'July','7':'Aug','8':'Sept','9':'Oct','10':'Nov','11':'Dec'};
+
+// module.exports = icons;
